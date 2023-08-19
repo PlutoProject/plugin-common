4f25bf65bf108e961bafb51320703ea1cb7c7c16
nostalfinals <yuu8583@proton.me>
2023-08-19 20:30:55 +0800

    build(connector): publications

--- pluto-connector/build.gradle.kts
+++ pluto-connector/build.gradle.kts
@@ -1,6 +1,10 @@
 group = "club.plutomc.plutoproject.common.connector"
 version = "3.0.0-SNAPSHOT"
 
+plugins {
+    id("maven-publish")
+}
+
 repositories {
     mavenCentral()
 }
@@ -18,4 +22,30 @@ dependencies {
     // Velocity
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
     kapt("com.velocitypowered:velocity-api:3.1.1")
+}
+
+publishing {
+    configure<PublishingExtension> {
+        publications.create<MavenPublication>(name) {
+            from(components["kotlin"])
+
+            groupId = "club.plutomc.plutoproject.common"
+            artifactId = "connector"
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            val releaseRepo = "https://nexus.nostaldaisuki.cc/repository/maven-releases/"
+            val snapshotRepo = "https://nexus.nostaldaisuki.cc/repository/maven-snapshots/"
+
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT")) snapshotRepo else releaseRepo)
+
+            credentials {
+                username = rootProject.properties["nostalRepoUsername"].toString()
+                password = rootProject.properties["nostalRepoPassword"].toString()
+            }
+        }
+    }
 }
