05784d8ee19786766b6f56eb06ed0112df6b6094
nostalfinals <yuu8583@proton.me>
2023-08-19 19:36:38 +0800

    chore(connector): suppress stupid warnings

--- pluto-connector/src/main/kotlin/club/plutomc/plutoproject/common/connector/plugin/velocity/VelocityConnectorPlugin.kt
+++ pluto-connector/src/main/kotlin/club/plutomc/plutoproject/common/connector/plugin/velocity/VelocityConnectorPlugin.kt
@@ -14,6 +14,7 @@ import java.nio.file.Path
 import java.util.logging.Logger
 import javax.inject.Inject
 
+@Suppress("UNUSED_PARAMETER")
 @Plugin(
     id = "pluto-connector",
     name = "pluto-connector"
