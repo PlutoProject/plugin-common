29407da154978a468e9ff93245d547383e67a674
nostalfinals <yuu8583@proton.me>
2023-08-23 17:41:01 +0800

    feat: 所有依赖统一管理

---
+++ pluto-runtime/src/main/kotlin/club/plutomc/plutoproject/common/runtime/BukkitRuntimePlugin.kt
@@ -0,0 +1,15 @@
+package club.plutomc.plutoproject.common.runtime
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BukkitRuntimePlugin: JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
