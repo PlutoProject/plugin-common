ad37c9d97134623cd5deabece4b896d298297904
nostalfinals <yuu8583@proton.me>
2023-08-23 17:57:47 +0800

    fix: 修改 Jedis 为 JedisPool

--- pluto-connector/src/main/kotlin/club/plutomc/plutoproject/common/connector/api/Connector.kt
+++ pluto-connector/src/main/kotlin/club/plutomc/plutoproject/common/connector/api/Connector.kt
@@ -2,10 +2,11 @@ package club.plutomc.plutoproject.common.connector.api
 
 import com.mongodb.client.MongoClient
 import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPool
 
 interface Connector {
 
-    val jedis: Jedis
+    val jedis: JedisPool
     val mongo: MongoClient
 
     fun close()
