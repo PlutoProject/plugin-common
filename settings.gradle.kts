692e8cf982e4a4fdd26245d9f1c27f42fc455cc6
nostalfinals <yuu8583@proton.me>
2023-09-18 09:36:34 +0800

    fix: 修改项目设置中的项目名称

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "plugin-common"
+rootProject.name = "pluto-plugins"
 include("pluto-connector")
 include("pluto-runtime")
 include("pluto-messaging")
